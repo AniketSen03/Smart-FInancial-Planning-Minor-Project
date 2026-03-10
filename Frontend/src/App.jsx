561ec058295cd2c6432ee2de48807b25d4828d1e
Aniket <aniketsen521@gmail.com>
2026-03-10 16:34:21 +0530

    fix: auth redirect issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import { Routes, Route, Navigate } from "react-router-dom";
 import SignIn from './components/SignIn';
 import AppInner from './components/AppInner';
 
 export default function App() {
+  const [token, setToken] = useState(() => localStorage.getItem('token'));
+
   return (
     <Routes>
-      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signin" element={<SignIn onLogin={() => setToken(localStorage.getItem('token'))} />} />
       <Route
         path="/"
         element={
-          localStorage.getItem('token')
+          token
             ? <ThemeProvider><AppInner /></ThemeProvider>
             : <Navigate to="/signin" replace />
         }
